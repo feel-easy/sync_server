09efcbacfe6d9fea08824d14afb2a1d79c835693
len <shenglei@veer.tv>
2022-11-18 13:53:39 +0800

    update

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/feel-easy/sync_server
+
+go 1.18
+
+require (
+	github.com/gofrs/uuid v4.3.1+incompatible // indirect
+	github.com/gomodule/redigo v1.8.9 // indirect
+	github.com/googollee/go-socket.io v1.6.2 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	socketio "github.com/googollee/go-socket.io"
+)
+
+func main() {
+	server := socketio.NewServer(nil)
+
+	server.OnConnect("/", func(s socketio.Conn) error {
+		s.SetContext("")
+		fmt.Println("connected:", s.ID(), s.RemoteAddr(), s.Rooms(), s.Namespace())
+		return nil
+	})
+
+	server.OnEvent("/", "msg", func(s socketio.Conn, msg string) {
+		fmt.Println("notice:", msg)
+		s.Emit("video-control", msg)
+	})
+
+	server.OnEvent("/", "video-control", func(s socketio.Conn, msg string) {
+		fmt.Println("notice:", msg)
+		s.Emit("video-control", msg)
+	})
+
+	server.OnEvent("/", "msg", func(s socketio.Conn, msg string) string {
+		s.SetContext(msg)
+		return "recv " + msg
+	})
+
+	server.OnEvent("/", "bye", func(s socketio.Conn) string {
+		last := s.Context().(string)
+		s.Emit("bye", last)
+		s.Close()
+		return last
+	})
+
+	server.OnError("/", func(s socketio.Conn, e error) {
+		fmt.Println("meet error:", e)
+	})
+
+	server.OnDisconnect("/", func(s socketio.Conn, reason string) {
+		fmt.Println("closed", reason)
+	})
+
+	go server.Serve()
+	defer server.Close()
+	// connection
+	http.Handle("/socket.io/", server)
+	http.Handle("/", http.FileServer(http.Dir("./asset")))
+	log.Println("Serving at localhost:8000...")
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
